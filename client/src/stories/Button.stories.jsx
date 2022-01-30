32994c6fa13d98c491e9ee76874a63683544d100
Konstancja Tanjga <kztanjga@gmail.com>
2022-01-30 22:05:24 +0100

    First commit

--- client/src/stories/Button.stories.jsx
+++ client/src/stories/Button.stories.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Button } from '../ui';
 import { Grid, Typography } from '@mui/material';
 
+
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Zadanie 1/Button',
@@ -34,6 +35,18 @@ export default {
         type: 'boolean',
       },
     },
+    startIcon: {
+      control: {
+        default: false,
+        type: 'boolean',
+      },
+    },
+    endIcon: {
+      control: {
+        default: false,
+        type: 'boolean',
+      },
+    },
   },
 };
 
@@ -41,38 +54,140 @@ export default {
 const Template = ({ label, ...args }) => <Button {...args}>{label}</Button>;
 const All = () => (
   <>
-    <Grid container>
-      <Grid item xs={12} sx={{ mb: 2 }}>
-        <Typography variant={'subheading'}>Contained</Typography>
-      </Grid>
-      <Button variant={'contained'} color={'primary'}>
-        Button
-      </Button>
-      <Button variant={'contained'} color={'error'}>
-        Button
-      </Button>
-      <Button variant={'contained'} color={'success'}>
-        Button
-      </Button>
-      <Button variant={'contained'} color={'warning'}>
-        Button
-      </Button>
-    </Grid>
     <Grid container sx={{ mt: 5 }}>
       <Grid item xs={12} sx={{ mb: 2 }}>
-        <Typography variant={'subheading'}>Outlined</Typography>
+      <b><Typography variant={'subheading'}>Primary</Typography>
+        </b>
+        <p><b>
+        <Typography variant={'subheading'}>Contained</Typography>
+        </b></p>
       </Grid>
-      <Button variant={'outlined'} color={'primary'}>
+      <Button variant="outlined" color='primary'>
         Button
       </Button>
-      <Button variant={'outlined'} color={'error'}>
+      <Button variant={'outlined'} variant={'contained'} color={'primary'}>
         Button
       </Button>
-      <Button variant={'outlined'} color={'success'}>
+      <Button
+  endIcon
+  size="large"
+  variant="contained"
+>
+  Button
+</Button>
+     <Button
+  disabled
+  endIcon
+  size="large"
+  startIcon
+  variant="contained"
+>
+  Button
+</Button>
+    </Grid>
+
+    <Grid container sx={{ mt: 5 }}>
+      <Grid item xs={12} sx={{ mb: 2 }}>
+        <p><b>
+        <Typography variant={'subheading'}>Outlined</Typography>
+        </b></p>
+      </Grid>
+      <Button variant="outlined" color='primary'>
         Button
       </Button>
-      <Button variant={'outlined'} color={'warning'}>
+      <Button variant={'outlined'} variant={'contained'} color={'primary'}>
         Button
+      </Button>
+      <Button onClick={() => {alert('clicked');}}>
+       Button
+     </Button>
+      <Button color='primary' variant="contained" disabled>
+       Disabled
+      </Button>
+    </Grid>
+
+  
+    <Grid container sx={{ mt: 5 }}>
+      <Grid item xs={12} sx={{ mb: 2 }}>
+      <b><Typography variant={'subheading'}>Error</Typography>
+        </b>
+        <p><b>
+        <Typography variant={'subheading'}>Contained</Typography>
+        </b></p>
+      </Grid>
+      <Button variant="outlined" color='error'>
+        Button
+      </Button>
+      <Button variant={'outlined'} variant={'contained'} color={'error'}>
+        Button
+      </Button>
+      <Button color={'error'} onClick={() => {alert('clicked');}}>
+       Button
+     </Button>
+      <Button color='error' variant="contained" disabled>
+       Disabled
+      </Button>
+    </Grid>
+
+    <Grid container sx={{ mt: 5 }}>
+      <Grid item xs={12} sx={{ mb: 2 }}>
+        <p><b>
+        <Typography variant={'subheading'}>Outlined</Typography>
+        </b></p>
+      </Grid>
+      <Button variant="outlined" color='error'>
+        Button
+      </Button>
+      <Button variant={'outlined'} variant={'contained'} color={'error'}>
+        Button
+      </Button>
+      <Button color={'error'} onClick={() => {alert('clicked');}}>
+       Button
+     </Button>
+      <Button color='error' variant="contained" disabled>
+       Disabled
+      </Button>
+    </Grid>
+
+    <Grid container sx={{ mt: 5 }}>
+      <Grid item xs={12} sx={{ mb: 2 }}>
+      <b><Typography variant={'subheading'}>Success</Typography>
+        </b>
+        <p><b>
+        <Typography variant={'subheading'}>Contained</Typography>
+        </b></p>
+      </Grid>
+      <Button variant="outlined" color='success'>
+        Button
+      </Button>
+      <Button variant={'outlined'} variant={'contained'} color={'success'}>
+        Button
+      </Button>
+      <Button color={'success'} onClick={() => {alert('clicked');}}>
+       Button
+     </Button>
+      <Button color='success' variant="contained" disabled>
+       Disabled
+      </Button>
+    </Grid>
+
+    <Grid container sx={{ mt: 5 }}>
+      <Grid item xs={12} sx={{ mb: 2 }}>
+        <p><b>
+        <Typography variant={'subheading'}>Contained</Typography>
+        </b></p>
+      </Grid>
+      <Button variant="outlined" color='success'>
+        Button
+      </Button>
+      <Button variant={'outlined'} variant={'contained'} color={'success'}>
+        Button
+      </Button>
+      <Button color={'success'} onClick={() => {alert('clicked');}}>
+       Button
+     </Button>
+      <Button color='success' variant="contained" disabled>
+       Disabled
       </Button>
     </Grid>
   </>
